c063021d5fd667535e79966dbceafdea67a1f556
taoying <2474671424@qq.com>
2025-02-15 14:24:41 +0800

    add copyright for pgvector

--- opengauss_sqlalchemy/utils/sparsevec.py
+++ opengauss_sqlalchemy/utils/sparsevec.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021-2025 Andrew Kane
+# <see AUTHORS file>
+#
+# Copyright (C) 2021-2022 Huawei Technologies Co.,Ltd.
+#
+# This module is part of pgvector-python and is released under
+# the MIT License: https://www.opensource.org/licenses/mit-license.php
+
 import numpy as np
 from struct import pack, unpack_from
 
